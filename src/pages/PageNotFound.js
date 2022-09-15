3e029e6683fb3f9d86e0ef2f45108fa72e78ad56
Sinan Şık <83316099+sinansk@users.noreply.github.com>
2022-09-15 21:41:33 +0300

    style-fix

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,6 @@
 const PageNotFound = () => {
   return (
-    <div className="min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto bg-center sm:bg-not-found-bg-orange sm:dark:bg-not-found-bg-blue bg-no-repeat bg-contain"></div>
+    <div className="min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto bg-center bg-not-found-bg-orange sm:dark:bg-not-found-bg-blue bg-no-repeat bg-contain"></div>
   );
 };
 
