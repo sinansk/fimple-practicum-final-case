4679ff380dbd8cb5a6ac0956a260adf0ffd8113b
Sinan Şık <83316099+sinansk@users.noreply.github.com>
2022-09-15 21:55:24 +0300

    style-fix

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <div className="min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto bg-center bg-not-found-bg-orange sm:dark:bg-not-found-bg-blue bg-no-repeat bg-contain">
-      <button className="px-2.5 py-1 mt-auto mb-3 btn btn-primary">
-        <Link to="/">ANA SAYFA</Link>
-      </button>
+      <div className="mt-auto mb-3">
+        <Link to="/">
+          <button className="px-2.5 py-1 btn btn-primary">ANA SAYFA</button>
+        </Link>
+      </div>
     </div>
   );
 };
