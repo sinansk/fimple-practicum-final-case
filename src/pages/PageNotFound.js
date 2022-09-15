9e2732ab8bbf515c7b0e67322f478e7f0697b8ec
Sinan Şık <83316099+sinansk@users.noreply.github.com>
2022-09-15 21:47:49 +0300

    style-fix

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 const PageNotFound = () => {
   return (
-    <div className="min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto bg-center bg-not-found-bg-orange sm:dark:bg-not-found-bg-blue bg-no-repeat bg-contain"></div>
+    <div className="min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto bg-center bg-not-found-bg-orange sm:dark:bg-not-found-bg-blue bg-no-repeat bg-contain">
+      <button className="px-2.5 py-1 mt-auto mb-3 btn btn-primary">
+        <Link to="/">ANA SAYFA</Link>
+      </button>
+    </div>
   );
 };
 
