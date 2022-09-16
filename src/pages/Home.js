e1d27c83f800fc709fb62091e60ff6d19c562406
Sinan Şık <83316099+sinansk@users.noreply.github.com>
2022-09-16 16:42:44 +0300

    style-fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="relative min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto animate-ltr-linear-right  ">
       <UserForm />
-      <div className="hidden absolute right-0 bottom-0 sm:flex sm:min-h-[calc(76vh)] max-h-fit max-w-fit flex-grow animate-ltr-image-right text-orange-500 dark:text-cyan-500">
+      <div className="xl:pt-24 hidden absolute right-0 bottom-0 sm:flex sm:min-h-[calc(76vh)] max-h-fit max-w-fit flex-grow animate-ltr-image-right text-orange-500 dark:text-cyan-500">
         <RightSvgComponent />
       </div>
     </div>
--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -5,7 +5,7 @@ const ResultPage = () => {
   return (
     <div className="relative min-h-screen sm:min-h-[calc(76vh)] flex flex-col justify-center items-center w-screen mx-auto animate-ltr-linear-left  ">
       <Result />
-      <div className="hidden text-orange-500 dark:text-cyan-500 absolute top-0 left-0 sm:flex sm:min-h-[calc(76vh)] flex-grow animate-ltr-image-left ">
+      <div className="xl:pt-24 hidden text-orange-500 dark:text-cyan-500 absolute top-0 left-0 sm:flex sm:min-h-[calc(76vh)] flex-grow animate-ltr-image-left ">
         <LeftSvgComponent />
       </div>
     </div>
